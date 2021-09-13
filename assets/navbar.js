8cff4de202bbb5fc177e6a68df1f53f65aad11a7
sauron65 <72841060+sauron65@users.noreply.github.com>
2021-09-13 11:01:51 -0700

    syntax changes
    `var` is deprecated. Use `const` or `let` instead. Also adds semicolons and other syntax changes

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,30 +1,30 @@
-var nav = document.createElement("nav")
+const nav = document.createElement("nav");
 nav.innerHTML = `
   <a class="logo" href="https://digital-electronics.w3spaces.com/">Digital Electronics</a>
   <a href="/Projects.html?bypass-cache=1629392725">Projects</a>
-  <a href = "/About.html?bypass-cache=1629392725l">About</a>
-  <a href= "/Join.html?bypass-cache=1629394689">Join</a>
-  <a href = "/people.html?bypass-cache=1629409447">People</a>
-  <a href = "/news.html?bypass-cache=1629424011">News</a>
-`
-document.body.appendChild(nav)
-var style = document.createElement("style")
+  <a href="/About.html?bypass-cache=1629392725l">About</a>
+  <a href="/Join.html?bypass-cache=1629394689">Join</a>
+  <a href="/people.html?bypass-cache=1629409447">People</a>
+  <a href="/news.html?bypass-cache=1629424011">News</a>
+`;
+document.body.appendChild(nav);
+const style = document.createElement("style");
 style.id = "navbarStyle"
 style.innerHTML = `
-nav{
+nav {
   background:var(--black);
 }
-nav > a{
+nav > a {
   padding:15px;
   color:white;
   display:inline-block;
   text-decoration:none;
 }
-nav > a:not(.logo){
+nav > a:not(.logo) {
   float:right;
 }
-nav > a:hover{
+nav > a:hover {
   background:var(--color-1);
 }
-`
-document.head.appendChild(style)
+`;
+document.head.appendChild(style);
